c8915e22444fe7271fda897590b9a49e04e034f7
Jameson Briggs <briggjam000@alpine.local>
2023-05-03 10:05:31 -0600

    updated the raycast and stuff

--- Assets/Skripts/ObjectExplosion.cs
+++ Assets/Skripts/ObjectExplosion.cs
@@ -9,15 +9,19 @@ public class ObjectExplosion : NetworkBehaviour
 {
     [SerializeField] private NetworkVariable<int> objectHealth = new NetworkVariable<int>(20);
     [SerializeField] private NetworkVariable<int> explosiveDamage = new NetworkVariable<int>(50);
-    [SerializeField] private NetworkVariable<int> explosiveRadius = new NetworkVariable<int>(15);
+    [SerializeField] private NetworkVariable<int> explosiveRadius = new NetworkVariable<int>(10);
     private GameObject explosive;
     private Rigidbody rigidbodyTemp;
+    private bool hasExploded = false;
 
     public void takeDamage(int damage) {
         explosive = transform.gameObject;
         //print("Kaboom?");
         objectHealth.Value -= damage;
-        if(objectHealth.Value <= 0 ){
+        if (objectHealth.Value < 0) {
+            objectHealth.Value = 0;
+        }
+        if(objectHealth.Value == 0 ){
             Explode(explosiveDamage.Value);
             ParticleSystem explodePart = GameObject.Find("ExplosionManager").GetComponent<ParticleSystem>();
             explodePart.transform.position = explosive.transform.position;
@@ -37,35 +41,40 @@ public class ObjectExplosion : NetworkBehaviour
         
     }
     public void Explode(int explosiveDamage) {
-        
+
+        if(hasExploded == true) {
+            return;
+        }
+        hasExploded = true;
+
         Collider[] explodeColliders = Physics.OverlapSphere(explosive.transform.position, explosiveRadius.Value);
         foreach (var explodeCollider in explodeColliders) {
+
             if(explodeCollider.gameObject.tag == "Player" || explodeCollider.gameObject.tag == "Destructible" || explodeCollider.gameObject.tag == "Explosive") {
-                print("Explosive Hit: " + explodeCollider);
                 Vector3 dir = (explosive.transform.position - explodeCollider.transform.position).normalized;
                 float dist = Vector3.Distance(explosive.transform.position, explodeCollider.transform.position);
 
-                if(Physics.Raycast(explosive.transform.position, -dir, dist - 1f) == false) {
-                    int disDa = Mathf.RoundToInt(dist);
-                    rigidbodyTemp = explodeCollider.transform.GetComponent<Rigidbody>();
-                    print(explosiveDamage + " " + disDa);
+                if(Physics.Raycast(explosive.transform.position, -dir, out RaycastHit hitEx, dist + 0.2f)) {
+                int disDa = Mathf.RoundToInt(dist);
+                rigidbodyTemp = explodeCollider.transform.GetComponent<Rigidbody>();
+                print(hitEx.collider);
 
-                    if(explodeCollider.transform.GetComponent<Collider>().tag == "Player") {
+                    if(hitEx.collider.tag == "Player") {
                         explodeCollider.transform.GetComponent<Collider>().GetComponent<PlayerStatsScript>().takeDamage(explosiveDamage - disDa);
                         rigidbodyTemp.AddForce(-dir * 60f * dist / 1.5f);
                     }
-                    if(explodeCollider.transform.GetComponent<Collider>().tag == "Destructible") {
+                    if(hitEx.collider.tag == "Destructible") {
                         explodeCollider.transform.GetComponent<Collider>().GetComponent<ObjectStatScript>().takeDamage(explosiveDamage - disDa);
                         rigidbodyTemp.AddForce(-dir * 60f * dist / 1.5f);
                     }
-                    if(explodeCollider.transform.GetComponent<Collider>().tag == "Explosive" && explodeCollider.gameObject != explosive) {
+                    if(hitEx.collider.tag == "Explosive" && explodeCollider.gameObject != explosive) {
                         explodeCollider.transform.GetComponent<Collider>().GetComponent<ObjectExplosion>().takeDamage(explosiveDamage - disDa);
-                        rigidbodyTemp.AddForce(-dir * 60f * dist / 1.5f);
-                        
+                        rigidbodyTemp.AddForce(-dir * 60f * dist / 1.5f); 
                     }
-                }
                 }
             }
         }
+    }
+        
 }
 
